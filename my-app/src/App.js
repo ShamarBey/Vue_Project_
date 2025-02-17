d0e88ff6121376be4a40d57533d531007976cdf8
Erik-Kipu <ervinmalyshev@gmail.com>
2025-02-18 01:53:34 +0300

    13

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	return (
-    <div>
-      <input />
-      <input />
-      <input />
-    </div>
-  );
+  const str1 = 'text1';
+	const str2 = 'text1';
+	
+	return <div>
+		<p>{str1}</p>
+		<p>{str2}</p>
+	</div>;
 }
 
 export default App;
