baed5bcdb2a4a5fd3eea828f4c18555f028c79f8
Erik-Kipu <ervinmalyshev@gmail.com>
2025-02-18 02:27:03 +0300

    22

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,19 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	const attr = 'block';
-	const li1 = <li>text1</li>;
-	const li2 = <li>text2</li>;
-	const li3 = <li>text3</li>;
-	const items = <><li>text1</li><li>text2</li><li>text3</li></>;
-	
-	return <div id={attr}>
-		text
-		{li1}
-		{li2}
-		{li3}
-		{items}
-	</div>;
-}
+	return (
+	  <div>
+		<input/>
+		<br />
+		<input/>
+		<br />
+		<input/>
+		<br />
+		<input/>
+	  </div>
+	);
+  }
 
 export default App;
