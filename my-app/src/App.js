f8873200eccef464a278174120cb1a66c5adca60
Erik-Kipu <ervinmalyshev@gmail.com>
2025-02-18 02:21:10 +0300

    20

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,12 +6,14 @@ function App() {
 	const li1 = <li>text1</li>;
 	const li2 = <li>text2</li>;
 	const li3 = <li>text3</li>;
+	const items = <><li>text1</li><li>text2</li><li>text3</li></>;
 	
 	return <div id={attr}>
 		text
 		{li1}
 		{li2}
 		{li3}
+		{items}
 	</div>;
 }
 
