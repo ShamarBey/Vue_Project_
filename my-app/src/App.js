59cb300fdf2168c97e729c522b7cce39eacab1e4
ShamarBey <rizaev.alim06@mail.ru>
2025-02-18 02:35:50 +0300

    23

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,15 +4,38 @@ import './App.css';
 function App() {
 	return (
 	  <div>
-		<input/>
-		<br />
-		<input/>
-		<br />
-		<input/>
-		<br />
-		<input/>
+		<ul>
+		  <li>Элемент 1</li>
+		  <li>Элемент 2</li>
+		  <li>Элемент 3</li>
+		  <li>Элемент 4</li>
+		  <li>Элемент 5</li>
+		  <li>Элемент 6</li>
+		  <li>Элемент 7</li>
+		  <li>Элемент 8</li>
+		  <li>Элемент 9</li>
+		  <li>Элемент 10</li>
+		</ul>
+		<table>
+			<tbody>
+        <tr>
+          <td>Ряд 1, Колонка 1</td>
+          <td>Ряд 1, Колонка 2</td>
+          <td>Ряд 1, Колонка 3</td>
+        </tr>
+        <tr>
+          <td>Ряд 2, Колонка 1</td>
+          <td>Ряд 2, Колонка 2</td>
+          <td>Ряд 2, Колонка 3</td>
+        </tr>
+        <tr>
+          <td>Ряд 3, Колонка 1</td>
+          <td>Ряд 3, Колонка 2</td>
+          <td>Ряд 3, Колонка 3</td>
+        </tr>
+		</tbody>
+      </table>
 	  </div>
 	);
-  }
-
+}
 export default App;
