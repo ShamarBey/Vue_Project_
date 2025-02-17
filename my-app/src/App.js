223736c9fba5e6c402c3e71d9b73688c5081dc03
ShamarBey <rizaev.alim06@mail.ru>
2025-02-18 02:17:37 +0300

    17-19

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	const obj = { name: 'john', surname: 'smit' };
-
-	return (
-	  <p>
-		name: <span>{obj.name}</span>, <br />
-		surname: <span>{obj.surname}</span>,
-	  </p>
-	);
+	const attr = 'block';
+	const li1 = <li>text1</li>;
+	const li2 = <li>text2</li>;
+	const li3 = <li>text3</li>;
+	
+	return <div id={attr}>
+		text
+		{li1}
+		{li2}
+		{li3}
+	</div>;
 }
 
 export default App;
