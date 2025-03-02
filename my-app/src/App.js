03215c4491cfd5a6e96456328fd6489bda6431a3
Erik-Kipu <ervinmalyshev@gmail.com>
2025-03-03 01:20:12 +0300

    Задание 37-43

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,61 +1,88 @@
 import React from 'react';
 
-// Функция для нахождения суммы цифр числа
-function getDigitsSum(number) {
-  return String(number)
-    .split('')
-    .reduce((sum, digit) => sum + Number(digit), 0);
-}
-
 function App() {
-  // Функции для обработки кликов
-  const show1 = () => {
-    alert(1);
-  };
+  // Задание 1: Массив с элементами <li>
+  const arr1 = [
+    <li key={1}>1</li>,
+    <li key={2}>2</li>,
+    <li key={3}>3</li>,
+    <li key={4}>4</li>,
+    <li key={5}>5</li>,
+  ];
 
-  const show2 = () => {
-    alert(2);
-  };
+  // Задание 2: Формирование списка с помощью цикла for
+  const arr2 = [];
+  for (let i = 1; i <= 5; i++) {
+    arr2.push(<li key={i}>{i}</li>);
+  }
 
-  const showNumber = (number) => {
-    alert(number);
-  };
+  // Задание 3: Массив с буквами
+  const arr3 = ['a', 'b', 'c', 'd', 'e'];
 
-  const handleClickEvent = (event) => {
-    console.log('Event:', event);
-  };
+  // Задание 4: Массив с пользователями
+  const users1 = [
+    { name: 'user1', surn: 'surn1', age: 30 },
+    { name: 'user2', surn: 'surn2', age: 31 },
+    { name: 'user3', surn: 'surn3', age: 32 },
+  ];
 
-  const handleClickTarget = (event) => {
-    console.log('Event Target:', event.target);
-  };
+  // Задание 5: Массив с пользователями и id
+  const users2 = [
+    { id: 1, name: 'user1', surn: 'surn1', age: 30 },
+    { id: 2, name: 'user2', surn: 'surn2', age: 31 },
+    { id: 3, name: 'user3', surn: 'surn3', age: 32 },
+  ];
 
   return (
     <div>
-      {/* Задание 1: Вывод суммы цифр числа 12345 */}
-      <p>Сумма цифр числа 12345: {getDigitsSum(12345)}</p>
+      {/* Задание 1: Рендеринг массива с элементами <li> */}
+      <h2>Задание 1</h2>
+      <ul>{arr1}</ul>
 
-      {/* Задание 2: Кнопки с вызовом функций show1 и show2 */}
-      <div>
-        <button onClick={show1}>act1</button>
-        <button onClick={show2}>act2</button>
-      </div>
+      {/* Задание 2: Рендеринг списка, сформированного циклом for */}
+      <h2>Задание 2</h2>
+      <ul>{arr2}</ul>
 
-      {/* Задание 3: Три кнопки с выводом чисел 1, 2, 3 */}
-      <div>
-        <button onClick={() => showNumber(1)}>act1</button>
-        <button onClick={() => showNumber(2)}>act2</button>
-        <button onClick={() => showNumber(3)}>act3</button>
-      </div>
+      {/* Задание 3: Рендеринг списка из массива букв */}
+      <h2>Задание 3</h2>
+      <ul>
+        {arr3.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
 
-      {/* Задание 4: Кнопка для вывода объекта Event в консоль */}
-      <div>
-        <button onClick={handleClickEvent}>Show Event</button>
-      </div>
+      {/* Задание 4: Рендеринг списка пользователей */}
+      <h2>Задание 4</h2>
+      <ul>
+        {users1.map((user, index) => (
+          <li key={index}>
+            {user.name} {user.surn}, возраст: {user.age}
+          </li>
+        ))}
+      </ul>
 
-      {/* Задание 5: Кнопка для вывода event.target в консоль */}
-      <div>
-        <button onClick={handleClickTarget}>Show Event Target</button>
-      </div>
+      {/* Задание 5: Рендеринг таблицы пользователей */}
+      <h2>Задание 5</h2>
+      <table border="1" cellPadding="5" cellSpacing="0">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Имя</th>
+            <th>Фамилия</th>
+            <th>Возраст</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users2.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.surn}</td>
+              <td>{user.age}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
