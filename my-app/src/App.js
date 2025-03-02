4f748d224aa640a98ae47dfc901cde9adf5b55b4
ShamarBey <rizaev.alim06@mail.ru>
2025-03-03 00:54:35 +0300

    Задание 27

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,41 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-	return (
-	  <div>
-		<ul>
-		  <li>Элемент 1</li>
-		  <li>Элемент 2</li>
-		  <li>Элемент 3</li>
-		  <li>Элемент 4</li>
-		  <li>Элемент 5</li>
-		  <li>Элемент 6</li>
-		  <li>Элемент 7</li>
-		  <li>Элемент 8</li>
-		  <li>Элемент 9</li>
-		  <li>Элемент 10</li>
-		</ul>
-		<table>
-			<tbody>
-        <tr>
-          <td>Ряд 1, Колонка 1</td>
-          <td>Ряд 1, Колонка 2</td>
-          <td>Ряд 1, Колонка 3</td>
-        </tr>
-        <tr>
-          <td>Ряд 2, Колонка 1</td>
-          <td>Ряд 2, Колонка 2</td>
-          <td>Ряд 2, Колонка 3</td>
-        </tr>
-        <tr>
-          <td>Ряд 3, Колонка 1</td>
-          <td>Ряд 3, Колонка 2</td>
-          <td>Ряд 3, Колонка 3</td>
-        </tr>
-		</tbody>
-      </table>
-	  </div>
-	);
+  const isAdmin = true; // Здесь может быть true или false
+
+  return (
+    <div>
+      {isAdmin && (
+        <div>
+          <p>Вы являетесь администратором.</p>
+          <p>Вам доступны все функции системы.</p>
+        </div>
+      )}
+    </div>
+  );
 }
+
 export default App;
