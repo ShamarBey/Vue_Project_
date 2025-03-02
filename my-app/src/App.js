da4022e0ffbacbc742872ad1b97de8814b493a21
Erik-Kipu <ervinmalyshev@gmail.com>
2025-03-03 02:23:51 +0300

    Задание 103

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,60 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+
+// Создание стилизованных компонентов
+const Container = styled.div`
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 20px;
+`;
+
+const Button = styled.button`
+  padding: 10px 20px;
+  margin: 5px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  background-color: ${(props) => (props.primary ? 'blue' : 'gray')};
+  color: white;
+
+  &:disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`;
 
 function ButtonComponent() {
-  // Переменные для CSS-свойств
-  const containerPadding = '20px';
-  const containerBorder = '1px solid #ccc';
-  const containerBorderRadius = '5px';
-  const containerMarginBottom = '20px';
-
-  const buttonPadding = '10px 20px';
-  const buttonMargin = '5px';
-  const buttonBorder = 'none';
-  const buttonBorderRadius = '5px';
-  const buttonCursor = 'pointer';
-
-  const primaryButtonBackgroundColor = 'blue';
-  const primaryButtonColor = 'white';
-
-  const secondaryButtonBackgroundColor = 'gray';
-  const secondaryButtonColor = 'white';
-
   return (
-    <div
-      style={{
-        padding: containerPadding,
-        border: containerBorder,
-        borderRadius: containerBorderRadius,
-        marginBottom: containerMarginBottom,
-      }}
-    >
-      <button
-        style={{
-          padding: buttonPadding,
-          margin: buttonMargin,
-          border: buttonBorder,
-          borderRadius: buttonBorderRadius,
-          cursor: buttonCursor,
-          backgroundColor: primaryButtonBackgroundColor,
-          color: primaryButtonColor,
-        }}
-      >
-        Кнопка 1
-      </button>
-      <button
-        style={{
-          padding: buttonPadding,
-          margin: buttonMargin,
-          border: buttonBorder,
-          borderRadius: buttonBorderRadius,
-          cursor: buttonCursor,
-          backgroundColor: secondaryButtonBackgroundColor,
-          color: secondaryButtonColor,
-        }}
-      >
-        Кнопка 2
-      </button>
-    </div>
+    <Container>
+      <Button primary>Кнопка 1</Button>
+      <Button>Кнопка 2</Button>
+    </Container>
   );
 }
 
