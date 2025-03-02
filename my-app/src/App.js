71c225c9dfd0540391feb5576556217ecc1ed01a
ShamarBey <rizaev.alim06@mail.ru>
2025-03-03 01:16:25 +0300

    Задание 32-35

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,18 +8,54 @@ function getDigitsSum(number) {
 }
 
 function App() {
-  const isAuth = true; // Здесь может быть true или false
-  const number = 123; // Число, сумму цифр которого нужно найти
-  const sum = getDigitsSum(number); // Вычисляем сумму цифр
+  // Функции для обработки кликов
+  const show1 = () => {
+    alert(1);
+  };
+
+  const show2 = () => {
+    alert(2);
+  };
+
+  const showNumber = (number) => {
+    alert(number);
+  };
+
+  const handleClickEvent = (event) => {
+    console.log('Event:', event);
+  };
+
+  const handleClickTarget = (event) => {
+    console.log('Event Target:', event.target);
+  };
 
   return (
     <div>
-      {/* Условный рендеринг для isAuth */}
-      {isAuth && <p>вы авторизованы</p>}
-      {!isAuth && <p>пожалуйста, авторизуйтесь</p>}
+      {/* Задание 1: Вывод суммы цифр числа 12345 */}
+      <p>Сумма цифр числа 12345: {getDigitsSum(12345)}</p>
 
-      {/* Вывод суммы цифр числа */}
-      <p>Сумма цифр числа {number} равна {sum}.</p>
+      {/* Задание 2: Кнопки с вызовом функций show1 и show2 */}
+      <div>
+        <button onClick={show1}>act1</button>
+        <button onClick={show2}>act2</button>
+      </div>
+
+      {/* Задание 3: Три кнопки с выводом чисел 1, 2, 3 */}
+      <div>
+        <button onClick={() => showNumber(1)}>act1</button>
+        <button onClick={() => showNumber(2)}>act2</button>
+        <button onClick={() => showNumber(3)}>act3</button>
+      </div>
+
+      {/* Задание 4: Кнопка для вывода объекта Event в консоль */}
+      <div>
+        <button onClick={handleClickEvent}>Show Event</button>
+      </div>
+
+      {/* Задание 5: Кнопка для вывода event.target в консоль */}
+      <div>
+        <button onClick={handleClickTarget}>Show Event Target</button>
+      </div>
     </div>
   );
 }
