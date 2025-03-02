408877d3935960473a5d5746f76a05772d97e5f2
Erik-Kipu <ervinmalyshev@gmail.com>
2025-03-03 02:26:07 +0300

    Задание 106

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,35 +2,32 @@ import React from 'react';
 import styled from 'styled-components';
 
 // Создание стилизованных компонентов
+const DIVA = styled.div`
+  width: 150px;
+  height: 150px;
+  background-color: yellow;
+  border: 2px solid black;
+`;
+
+const DIVB = styled(DIVA)`
+  background-color: green;
+  border-width: 3px;
+`;
+
 const Container = styled.div`
+  display: flex;
+  gap: 10px;
   padding: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  margin-bottom: 20px;
 `;
 
-const Button = styled.button`
-  padding: 10px 20px;
-  margin: 5px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  background-color: ${(props) => (props.primary ? 'blue' : 'gray')};
-  color: white;
-
-  &:disabled {
-    opacity: 0.5;
-    cursor: not-allowed;
-  }
-`;
-
-function ButtonComponent() {
+function Block1() {
   return (
     <Container>
-      <Button primary>Кнопка 1</Button>
-      <Button>Кнопка 2</Button>
+      <DIVA />
+      <DIVB />
+      <DIVA />
     </Container>
   );
 }
 
-export default ButtonComponent;
+export default Block1;
